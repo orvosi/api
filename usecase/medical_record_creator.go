749d924adc329792d71a181371027d30da782557
Indra Saputra <flame.fire99@gmail.com>
2021-01-05 12:31:57 +0700

    validate if medical record empty

--- usecase/medical_record_creator.go
+++ usecase/medical_record_creator.go
@@ -12,3 +12,22 @@ type CreateMedicalRecord interface {
 	// Create creates a new medical record.
 	Create(ctx context.Context, record *entity.MedicalRecord) *entity.Error
 }
+
+// MedicalRecordCreator responsibles for medical record creation workflow.
+type MedicalRecordCreator struct {
+}
+
+// Create creates a new medical record and persist it into a storage.s
+func (mrc *MedicalRecordCreator) Create(ctx context.Context, record *entity.MedicalRecord) *entity.Error {
+	if err := validateMedicalRecord(record); err != nil {
+		return err
+	}
+	return nil
+}
+
+func validateMedicalRecord(record *entity.MedicalRecord) *entity.Error {
+	if record == nil {
+		return entity.ErrEmptyMedicalRecord
+	}
+	return nil
+}
