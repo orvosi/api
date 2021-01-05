4262ef51e77708ebf1e8ed2429f4a5d5518df955
Indra Saputra <flame.fire99@gmail.com>
2021-01-05 15:47:18 +0700

    implement insert medical record to database

--- internal/repository/medical_record_inserter.go
+++ internal/repository/medical_record_inserter.go
@@ -1,6 +1,13 @@
 package repository
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	"github.com/indrasaputra/hashids"
+	"github.com/orvosi/api/entity"
+)
 
 // MedicalRecordInserter connects the database with medical record entity
 // and only responsible for inserting a new data.
@@ -11,4 +18,35 @@ type MedicalRecordInserter struct {
 // NewMedicalRecordInserter creates an instance of MedicalRecordInserter.
 func NewMedicalRecordInserter(db *sql.DB) *MedicalRecordInserter {
 	return &MedicalRecordInserter{db: db}
+}
+
+// Insert inserts a new medical record data into the database.
+func (mri *MedicalRecordInserter) Insert(ctx context.Context, record *entity.MedicalRecord) *entity.Error {
+	if record == nil {
+		return entity.ErrEmptyMedicalRecord
+	}
+
+	query := "INSERT INTO " +
+		"medical_records (user_id, symptom, diagnosis, therapy, created_at, updated_at, created_by, updated_by) " +
+		"VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id"
+
+	row := mri.db.QueryRow(query,
+		record.User.ID,
+		record.Symptom,
+		record.Diagnosis,
+		record.Therapy,
+		time.Now(),
+		time.Now(),
+		record.User.Email,
+		record.User.Email,
+	)
+
+	var id uint64
+	err := row.Scan(&id)
+	if err != nil {
+		return entity.WrapError(entity.ErrInternalServer, "[MedicalRecordInserter-Insert] exec insert query: "+err.Error())
+	}
+
+	record.ID = hashids.ID(id)
+	return nil
 }
