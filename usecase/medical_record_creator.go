b555d0fbe4b68ab0f4ecf675b3675eb5bc35f16a
Indra Saputra <flame.fire99@gmail.com>
2021-01-05 11:52:50 +0700

    interface for create medical record

--- usecase/medical_record_creator.go
+++ usecase/medical_record_creator.go
@@ -1,1 +1,14 @@
 package usecase
+
+import (
+	"context"
+
+	"github.com/orvosi/api/entity"
+)
+
+// CreateMedicalRecord defines the business logic
+// to create a medical record.
+type CreateMedicalRecord interface {
+	// Create creates a new medical record.
+	Create(ctx context.Context, record *entity.MedicalRecord) error
+}
