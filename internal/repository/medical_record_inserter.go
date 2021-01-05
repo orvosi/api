a29dd41be22c58666a9c8f0f1a1821ac95825dc4
Indra Saputra <flame.fire99@gmail.com>
2021-01-05 15:42:31 +0700

    create medical record inserter struct and its constructor

--- internal/repository/medical_record_inserter.go
+++ internal/repository/medical_record_inserter.go
@@ -1,1 +1,14 @@
 package repository
+
+import "database/sql"
+
+// MedicalRecordInserter connects the database with medical record entity
+// and only responsible for inserting a new data.
+type MedicalRecordInserter struct {
+	db *sql.DB
+}
+
+// NewMedicalRecordInserter creates an instance of MedicalRecordInserter.
+func NewMedicalRecordInserter(db *sql.DB) *MedicalRecordInserter {
+	return &MedicalRecordInserter{db: db}
+}
