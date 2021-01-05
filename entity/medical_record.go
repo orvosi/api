1e9730a4aed8501801c08f41f337ef15ed85f40c
Indra Saputra <flame.fire99@gmail.com>
2021-01-05 11:40:05 +0700

    create medical record entity

--- entity/medical_record.go
+++ entity/medical_record.go
@@ -1,1 +1,12 @@
 package entity
+
+import "github.com/indrasaputra/hashids"
+
+// MedicalRecord holds the user's medical record data
+type MedicalRecord struct {
+	ID        hashids.ID `json:"id"`
+	Symptom   string     `json:"symptom"`
+	Diagnosis string     `json:"diagnosis"`
+	Therapy   string     `json:"therapy"`
+	Result    string     `json:"result"`
+}
