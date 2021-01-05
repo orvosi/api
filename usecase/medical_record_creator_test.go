755c185bf88172f570acd7d8e667bd6084970bed
Indra Saputra <flame.fire99@gmail.com>
2021-01-05 15:12:31 +0700

    test medical record creator when medical record is empty

--- usecase/medical_record_creator_test.go
+++ usecase/medical_record_creator_test.go
@@ -1,9 +1,11 @@
 package usecase_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	"github.com/orvosi/api/entity"
 	mock_usecase "github.com/orvosi/api/test/mock/usecase"
 	"github.com/orvosi/api/usecase"
 	"github.com/stretchr/testify/assert"
@@ -21,6 +23,20 @@ func TestNewMedicalRecordCreator(t *testing.T) {
 	t.Run("successfully create an instance of MedicalRecordCreator", func(t *testing.T) {
 		exec := createMedicalRecordCreatorExecutor(ctrl)
 		assert.NotNil(t, exec.usecase)
+	})
+}
+
+func TestMedicalRecordCreator_Create(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	t.Run("medical record entity is empty/nil", func(t *testing.T) {
+		exec := createMedicalRecordCreatorExecutor(ctrl)
+
+		err := exec.usecase.Create(context.Background(), nil)
+
+		assert.NotNil(t, err)
+		assert.Equal(t, entity.ErrEmptyMedicalRecord, err)
 	})
 }
 
