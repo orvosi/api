4044aec4827e9e5e2c07c36f6372e89a29f29653
Indra Saputra <flame.fire99@gmail.com>
2021-01-05 11:56:56 +0700

    create error entity

---
+++ entity/error.go
@@ -0,0 +1,37 @@
+package entity
+
+import "fmt"
+
+// Error represents a data structure for error.
+type Error struct {
+	// Code represents error code.
+	Code string `json:"code"`
+	// Message represents error message.
+	// This is the message that exposed to the user.
+	Message string `json:"message"`
+	// internalMessage represents deep error message.
+	// This is should not be exposed to the user directly.
+	// This attributes should be used as log.
+	internalMessage string
+}
+
+// NewError creates an instance of Error.
+func NewError(code, message string) *Error {
+	return &Error{
+		Code:    code,
+		Message: message,
+	}
+}
+
+// Error returns concatenation of public message and internal message in one string.
+func (err *Error) Error() string {
+	return fmt.Sprintf("%s. %s", err.Message, err.internalMessage)
+}
+
+// WrapError wraps Error with given message.
+// The message will be put in internalMessage attribute
+// and can be accessed via Error() method.
+func WrapError(err *Error, message string) *Error {
+	err.internalMessage = fmt.Sprintf("%s. %s", err.internalMessage, message)
+	return err
+}
