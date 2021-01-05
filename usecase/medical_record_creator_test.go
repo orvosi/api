4969fb892e279c8cac5b3653a98eaed9f3ac0628
Indra Saputra <flame.fire99@gmail.com>
2021-01-05 15:09:07 +0700

    create medical record creator executor

--- usecase/medical_record_creator_test.go
+++ usecase/medical_record_creator_test.go
@@ -1,1 +1,11 @@
 package usecase_test
+
+import (
+	mock_usecase "github.com/orvosi/api/test/mock/usecase"
+	"github.com/orvosi/api/usecase"
+)
+
+type MedicalRecordCreator_Executor struct {
+	usecase  *usecase.MedicalRecordCreator
+	inserter *mock_usecase.MockMedicalRecordInserter
+}
