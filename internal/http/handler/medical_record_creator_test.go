152cc8a5ceae912d67f967515750b48fc6eda7ef
Indra Saputra <flame.fire99@gmail.com>
2021-01-22 18:23:27 +0700

    test when request body is valid but the content doesnt follow the rule

--- internal/http/handler/medical_record_creator_test.go
+++ internal/http/handler/medical_record_creator_test.go
@@ -2,15 +2,20 @@ package handler_test
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/indrasaputra/hashids"
+	"github.com/orvosi/api/entity"
+
 	"github.com/golang/mock/gomock"
 	"github.com/labstack/echo/v4"
 	"github.com/orvosi/api/internal/http/handler"
+	"github.com/orvosi/api/internal/http/middleware"
 	mock_usecase "github.com/orvosi/api/test/mock/usecase"
 	"github.com/stretchr/testify/assert"
 )
@@ -68,6 +73,27 @@ func TestMedicalRecordCreator_Create(t *testing.T) {
 		str := fmt.Sprintf("%s\n", `{"errors":[{"code":"01-001","message":"Internal server error"}],"meta":null}`)
 		assert.Equal(t, str, rec.Body.String())
 	})
+
+	t.Run("creator service returns 4xx error", func(t *testing.T) {
+		mr := createValidCreateMedicalRecordRequest()
+		body, _ := json.Marshal(mr)
+		req := httptest.NewRequest(http.MethodPost, "/medical-records", bytes.NewBuffer(body))
+		req.Header.Set("Content-Type", echo.MIMEApplicationJSON)
+		user := createUserInformation()
+		req = req.WithContext(context.WithValue(context.Background(), middleware.ContextKeyUser, user))
+
+		rec := httptest.NewRecorder()
+		e := echo.New()
+		ctx := e.NewContext(req, rec)
+
+		exec := createMedicalRecordCreatorExecutor(ctrl)
+		exec.usecase.EXPECT().Create(ctx.Request().Context(), createMedicalRecordFromRequest(mr, user)).Return(entity.ErrInvalidMedicalRecordAttribute)
+		exec.handler.Create(ctx)
+
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+		str := fmt.Sprintf("%s\n", `{"errors":[{"code":"02-002","message":"Medical record's attributes are invalid. Please, check all attributes"}],"meta":null}`)
+		assert.Equal(t, str, rec.Body.String())
+	})
 }
 
 func createValidCreateMedicalRecordRequest() *handler.MedicalRecordRequest {
@@ -75,6 +101,24 @@ func createValidCreateMedicalRecordRequest() *handler.MedicalRecordRequest {
 		Symptom:   "symptom",
 		Diagnosis: "diagnosis",
 		Therapy:   "therapy",
+	}
+}
+
+func createUserInformation() *entity.User {
+	return &entity.User{
+		ID:       hashids.ID(1),
+		Email:    "user@email.com",
+		Name:     "Edward Jenner",
+		GoogleID: "12345678901234567890",
+	}
+}
+
+func createMedicalRecordFromRequest(req *handler.MedicalRecordRequest, user *entity.User) *entity.MedicalRecord {
+	return &entity.MedicalRecord{
+		User:      user,
+		Symptom:   req.Symptom,
+		Diagnosis: req.Diagnosis,
+		Therapy:   req.Therapy,
 	}
 }
 
