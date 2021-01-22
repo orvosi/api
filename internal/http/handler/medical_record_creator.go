ae8db5609be5e312bc62ef95da2ecc49cab4302f
Indra Saputra <flame.fire99@gmail.com>
2021-01-22 15:07:53 +0700

    create medical record creator struct and constructor

--- internal/http/handler/medical_record_creator.go
+++ internal/http/handler/medical_record_creator.go
@@ -1,9 +1,26 @@
 package handler
 
+import (
+	"github.com/orvosi/api/usecase"
+)
+
 // MedicalRecordRequest represents medical record request.
 type MedicalRecordRequest struct {
 	Symptom   string `json:"symptom"`
 	Diagnosis string `json:"diagnosis"`
 	Therapy   string `json:"therapy"`
 	Result    string `json:"result"`
+}
+
+// MedicalRecordCreator handles HTTP request and response
+// for create medical record.
+type MedicalRecordCreator struct {
+	creator usecase.MedicalRecordCreator
+}
+
+// NewMedicalRecordCreator creates an instance of MedicalCreator.
+func NewMedicalRecordCreator(creator usecase.MedicalRecordCreator) *MedicalRecordCreator {
+	return &MedicalRecordCreator{
+		creator: creator,
+	}
 }
