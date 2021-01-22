f53eeec1bb2c1c6b68937848cb8ff2958d5bb2ee
Indra Saputra <flame.fire99@gmail.com>
2021-01-22 15:09:45 +0700

    framework func for create

--- internal/http/handler/medical_record_creator.go
+++ internal/http/handler/medical_record_creator.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"github.com/labstack/echo/v4"
 	"github.com/orvosi/api/usecase"
 )
 
@@ -23,4 +24,9 @@ func NewMedicalRecordCreator(creator usecase.MedicalRecordCreator) *MedicalRecor
 	return &MedicalRecordCreator{
 		creator: creator,
 	}
+}
+
+// Create handles `POST /medical-records` endpoint.
+func (mrc *MedicalRecordCreator) Create(ctx echo.Context) error {
+	return nil
 }
