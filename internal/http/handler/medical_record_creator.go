2f5f4a45f4e1edf55b75c32707818af181c82993
Indra Saputra <flame.fire99@gmail.com>
2021-01-22 15:05:14 +0700

    create medical record request

--- internal/http/handler/medical_record_creator.go
+++ internal/http/handler/medical_record_creator.go
@@ -1,1 +1,9 @@
 package handler
+
+// MedicalRecordRequest represents medical record request.
+type MedicalRecordRequest struct {
+	Symptom   string `json:"symptom"`
+	Diagnosis string `json:"diagnosis"`
+	Therapy   string `json:"therapy"`
+	Result    string `json:"result"`
+}
