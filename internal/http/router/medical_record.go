7bdeece434f121cd56da15bf7183dba288999f18
Indra Saputra <flame.fire99@gmail.com>
2021-01-25 14:52:14 +0700

    create route for POST medical record

--- internal/http/router/medical_record.go
+++ internal/http/router/medical_record.go
@@ -1,1 +1,21 @@
 package router
+
+import (
+	"net/http"
+
+	"github.com/orvosi/api/internal/http/handler"
+)
+
+// MedicalRecordCreator creates routes for medical record creator.
+func MedicalRecordCreator(h *handler.MedicalRecordCreator) []*Route {
+	var routes []*Route
+
+	r := &Route{
+		Method:  http.MethodPost,
+		Path:    "/medical-records",
+		Handler: h.Create,
+	}
+
+	routes = append(routes, r)
+	return routes
+}
