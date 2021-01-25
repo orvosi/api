ec0d6f2a368e2a7bc5666698637e707ae95c4a63
Indra Saputra <flame.fire99@gmail.com>
2021-01-25 15:53:30 +0700

    test when build db using unknown driver

--- internal/builder/database_test.go
+++ internal/builder/database_test.go
@@ -1,1 +1,21 @@
 package builder_test
+
+import (
+	"testing"
+
+	"github.com/orvosi/api/internal/builder"
+	"github.com/orvosi/api/internal/config"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuildSQLDatabase(t *testing.T) {
+	t.Run("fail to build sql.DB due to unknown driver", func(t *testing.T) {
+		cfg, err := config.NewConfig("../../test/fixture/env.valid")
+		assert.Nil(t, err)
+
+		db, err := builder.BuildSQLDatabase("unknown", cfg)
+
+		assert.NotNil(t, err)
+		assert.Nil(t, db)
+	})
+}
