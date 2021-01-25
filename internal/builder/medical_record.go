64e36c5171d67a227032f6e481a61d1bc636403c
Indra Saputra <flame.fire99@gmail.com>
2021-01-25 15:46:09 +0700

    create builder for medical record creator

--- internal/builder/medical_record.go
+++ internal/builder/medical_record.go
@@ -1,1 +1,20 @@
 package builder
+
+import (
+	"database/sql"
+
+	"github.com/orvosi/api/internal/config"
+	"github.com/orvosi/api/internal/http/handler"
+	"github.com/orvosi/api/internal/http/router"
+	"github.com/orvosi/api/internal/repository"
+	"github.com/orvosi/api/usecase"
+)
+
+// BuildMedicalRecordCreator build medical record creation workflow
+// starting from handler down to repository.
+func BuildMedicalRecordCreator(cfg *config.Config, db *sql.DB) []*router.Route {
+	ins := repository.NewMedicalRecordInserter(db)
+	uc := usecase.NewMedicalRecordCreator(ins)
+	hdr := handler.NewMedicalRecordCreator(uc)
+	return router.MedicalRecordCreator(hdr)
+}
