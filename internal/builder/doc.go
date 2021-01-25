1712a8bd0af575ec6ca5b420bb5e7f51f03e932d
Indra Saputra <flame.fire99@gmail.com>
2021-01-25 15:28:11 +0700

    create builder package

---
+++ internal/builder/doc.go
@@ -0,0 +1,5 @@
+// Package builder provides functionality to build
+// representative flow.
+// Imagine this package as an implementation of builder design pattern.
+// Read more: https://sourcemaking.com/design_patterns/builder.
+package builder
