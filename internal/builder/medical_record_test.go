11611f34cf068bd6ce1532ba68048d40be5e826c
Indra Saputra <flame.fire99@gmail.com>
2021-01-25 15:48:09 +0700

    test successfully build medical creator from builder

--- internal/builder/medical_record_test.go
+++ internal/builder/medical_record_test.go
@@ -1,1 +1,22 @@
 package builder_test
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/orvosi/api/internal/builder"
+	"github.com/orvosi/api/internal/config"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuildMedicalRecordCreator(t *testing.T) {
+	t.Run("successfully build medical record creator", func(t *testing.T) {
+		cfg, err := config.NewConfig("../../test/fixture/env.valid")
+		assert.Nil(t, err)
+
+		db := &sql.DB{}
+
+		routes := builder.BuildMedicalRecordCreator(cfg, db)
+		assert.NotEmpty(t, routes)
+	})
+}
