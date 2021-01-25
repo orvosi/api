1caf7728afef86daa083363d15dd49ab26f76209
Indra Saputra <flame.fire99@gmail.com>
2021-01-25 15:32:36 +0700

    create struct to hold project and db config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,19 @@
 package config
+
+// Database holds configuration for database.
+type Database struct {
+	Host         string `env:"DATABASE_HOST,default=localhost"`
+	Port         string `env:"DATABASE_PORT,default=5432"`
+	Username     string `env:"DATABASE_USERNAME,required"`
+	Password     string `env:"DATABASE_PASSWORD,required"`
+	Name         string `env:"DATABASE_NAME,required"`
+	SSLMode      string `env:"DATABASE_SSL_MODE,default=disable"`
+	MaxOpenConns int    `env:"DATABASE_MAX_OPEN_CONNS,default=5"`
+	MaxIdleConns int    `env:"DATABASE_MAX_IDLE_CONNS,default=1"`
+}
+
+// Config holds configuration for the project.
+type Config struct {
+	Port     string `env:"PORT,default=6666"`
+	Database Database
+}
