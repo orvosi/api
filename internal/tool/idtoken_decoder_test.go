9bfd52806fb2ad2f3a16fdff3e02b4a1dcba0fcb
Indra Saputra <flame.fire99@gmail.com>
2021-01-26 14:45:36 +0700

    create jwt mock struct

--- internal/tool/idtoken_decoder_test.go
+++ internal/tool/idtoken_decoder_test.go
@@ -23,6 +23,30 @@ var (
 	 }`)
 )
 
+type payload struct {
+	Issuer   string                 `json:"iss"`
+	Audience string                 `json:"aud"`
+	Expires  int64                  `json:"exp"`
+	IssuedAt int64                  `json:"iat"`
+	Subject  string                 `json:"sub,omitempty"`
+	Claims   map[string]interface{} `json:"-"`
+}
+
+// jwt represents the segments of a jwt and exposes convenience methods for
+// working with the different segments.
+type jwt struct {
+	header    string
+	payload   string
+	signature string
+}
+
+// jwtHeader represents a parted jwt's header segment.
+type jwtHeader struct {
+	Algorithm string `json:"alg"`
+	Type      string `json:"typ"`
+	KeyID     string `json:"kid"`
+}
+
 func TestNewIDTokenDecoder(t *testing.T) {
 	t.Run("fail to create an instance of IDTokenDecoder due to invalid credentials", func(t *testing.T) {
 		dec, err := tool.NewIDTokenDecoder(audience, []byte(`invalid`))
@@ -36,5 +60,11 @@ func TestNewIDTokenDecoder(t *testing.T) {
 
 		assert.Nil(t, err)
 		assert.NotNil(t, dec)
+	})
+}
+
+func TestIDTokenDecoder_Decode(t *testing.T) {
+	t.Run("fail to decode id token due to invalid audience", func(t *testing.T) {
+
 	})
 }
