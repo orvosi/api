a2b5035a098aa662485216f9984fc3ad21ee673d
Indra Saputra <flame.fire99@gmail.com>
2021-01-26 16:28:32 +0700

    create medical record finder, its constructor, and selector interface

--- usecase/medical_record_finder.go
+++ usecase/medical_record_finder.go
@@ -12,3 +12,22 @@ type FindMedicalRecord interface {
 	// Find finds medical records that belong to specific user (based on email).
 	FindByEmail(ctx context.Context, email string) ([]*entity.MedicalRecord, *entity.Error)
 }
+
+// MedicalRecordSelector defines the business logic
+// to select or find medical record data from storage.
+type MedicalRecordSelector interface {
+	// FindByEmail finds all medical records bounded to specific email.
+	FindByEmail(ctx context.Context, email string) ([]*entity.MedicalRecord, *entity.Error)
+}
+
+// MedicalRecordFinder responsibles for medical record find workflow.
+type MedicalRecordFinder struct {
+	selector MedicalRecordSelector
+}
+
+// NewMedicalRecordFinder creates an instance of MedicalRecordFinder.
+func NewMedicalRecordFinder(selector MedicalRecordSelector) *MedicalRecordFinder {
+	return &MedicalRecordFinder{
+		selector: selector,
+	}
+}
