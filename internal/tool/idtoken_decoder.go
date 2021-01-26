52eb84987e2087142fd5bfe0343594b83fc32dd2
Indra Saputra <flame.fire99@gmail.com>
2021-01-26 14:38:30 +0700

    implement decode function

--- internal/tool/idtoken_decoder.go
+++ internal/tool/idtoken_decoder.go
@@ -3,6 +3,7 @@ package tool
 import (
 	"context"
 
+	"github.com/orvosi/api/entity"
 	"github.com/pkg/errors"
 	"google.golang.org/api/idtoken"
 )
@@ -27,4 +28,27 @@ func NewIDTokenDecoder(audience string, credential []byte) (*IDTokenDecoder, err
 		validator: v,
 		audience:  audience,
 	}, nil
+}
+
+// Decode decodes google token.
+func (id *IDTokenDecoder) Decode(ctx context.Context, googleToken string) (*entity.User, *entity.Error) {
+	payload, err := id.validator.Validate(ctx, googleToken, id.audience)
+	if err != nil {
+		return nil, entity.WrapError(entity.ErrUnauthorized, err.Error())
+	}
+
+	user := payloadToUser(payload)
+	return user, nil
+}
+
+func payloadToUser(payload *idtoken.Payload) *entity.User {
+	id, _ := payload.Claims["sub"].(string)
+	name, _ := payload.Claims["name"].(string)
+	email, _ := payload.Claims["email"].(string)
+
+	return &entity.User{
+		GoogleID: id,
+		Name:     name,
+		Email:    email,
+	}
 }
