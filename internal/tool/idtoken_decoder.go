787de753debfd5def6804724e2016bd58e7925aa
Indra Saputra <flame.fire99@gmail.com>
2021-01-26 12:36:12 +0700

    constructor for idtokendecoder

--- internal/tool/idtoken_decoder.go
+++ internal/tool/idtoken_decoder.go
@@ -1,9 +1,30 @@
 package tool
 
-import "google.golang.org/api/idtoken"
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"google.golang.org/api/idtoken"
+)
 
 // IDTokenDecoder responsibles for decoding google's ID token.
 type IDTokenDecoder struct {
 	validator *idtoken.Validator
 	audience  string
+}
+
+// NewIDTokenDecoder creates an instance of IDTokenDecoder.
+func NewIDTokenDecoder(audience string, credential []byte) (*IDTokenDecoder, error) {
+	v, err := idtoken.NewValidator(
+		context.Background(),
+		idtoken.WithCredentialsJSON(credential),
+	)
+	if err != nil {
+		return nil, errors.Wrap(err, "[NewIDTokenDecoder] fail to create IDTokenDecoder")
+	}
+
+	return &IDTokenDecoder{
+		validator: v,
+		audience:  audience,
+	}, nil
 }
