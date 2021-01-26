71f063256807e14d0eaeac0ba0c7e8a23638c476
Indra Saputra <flame.fire99@gmail.com>
2021-01-26 11:36:01 +0700

    test JWTDecoder when request doesnt contain auth

--- internal/http/middleware/middleware_test.go
+++ internal/http/middleware/middleware_test.go
@@ -1,1 +1,49 @@
 package middleware_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/orvosi/api/entity"
+	"github.com/orvosi/api/internal/http/middleware"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWithJWTDecoder(t *testing.T) {
+	t.Run("request doesn't contain authorization", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		rec := httptest.NewRecorder()
+
+		e := echo.New()
+		ctx := e.NewContext(req, rec)
+
+		hdr := createHandler()
+		dec := createNormalDecoder()
+		hdr = middleware.WithJWTDecoder(dec)(hdr)
+
+		err := hdr(ctx)
+
+		assert.NotNil(t, err)
+		assert.Equal(t, entity.ErrUnauthorized, err)
+	})
+}
+
+func createErrorDecoder() middleware.JWTDecoder {
+	return func(token string) (*entity.User, *entity.Error) {
+		return nil, entity.ErrInternalServer
+	}
+}
+
+func createNormalDecoder() middleware.JWTDecoder {
+	return func(token string) (*entity.User, *entity.Error) {
+		return &entity.User{}, nil
+	}
+}
+
+func createHandler() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.String(http.StatusOK, "test")
+	}
+}
