c27c236c2dc1ddbe64dcd24749a587ecd9c96add
Indra Saputra <flame.fire99@gmail.com>
2021-01-27 15:10:26 +0700

    implement find by email from db

--- internal/repository/medical_record_selector.go
+++ internal/repository/medical_record_selector.go
@@ -1,6 +1,12 @@
 package repository
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"log"
+
+	"github.com/orvosi/api/entity"
+)
 
 // MedicalRecordSelector connects the database with medical record entity
 // and only responsible for retrieving medical record data.
@@ -11,4 +17,26 @@ type MedicalRecordSelector struct {
 // NewMedicalRecordSelector creates an instance of MedicalRecordSelector.
 func NewMedicalRecordSelector(db *sql.DB) *MedicalRecordSelector {
 	return &MedicalRecordSelector{db: db}
+}
+
+// FindByEmail finds all medical records bounded to specific email.
+func (ms *MedicalRecordSelector) FindByEmail(ctx context.Context, email string) ([]*entity.MedicalRecord, *entity.Error) {
+	query := "SELECT id, symptom, diagnosis, therapy, result, updated_at FROM medical_records WHERE email = $1 ORDER BY id ASC"
+	rows, err := ms.db.QueryContext(ctx, query, email)
+	if err != nil {
+		return []*entity.MedicalRecord{}, entity.WrapError(entity.ErrInternalServer, err.Error())
+	}
+	defer rows.Close()
+
+	var result []*entity.MedicalRecord
+	for rows.Next() {
+		var tmp entity.MedicalRecord
+		if err := rows.Scan(&tmp.ID, &tmp.Symptom, &tmp.Diagnosis, &tmp.Therapy, &tmp.Result, &tmp.UpdatedAt); err != nil {
+			log.Printf("[MedicalRecordSelector-FindByEmail] scan rows error: %v", err)
+			continue
+		}
+
+		result = append(result, &tmp)
+	}
+	return result, nil
 }
