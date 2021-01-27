a2b8879a21e7441e702da8bdecf4e3b683f00c64
Indra Saputra <flame.fire99@gmail.com>
2021-01-27 15:15:04 +0700

    test medicalrecordselector constructor

--- internal/repository/medical_record_selector_test.go
+++ internal/repository/medical_record_selector_test.go
@@ -1,1 +1,35 @@
 package repository_test
+
+import (
+	"log"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/orvosi/api/internal/repository"
+	"github.com/stretchr/testify/assert"
+)
+
+type MedicalRecordSelector_Executor struct {
+	repo *repository.MedicalRecordSelector
+	sql  sqlmock.Sqlmock
+}
+
+func TestNewMedicalRecordSelector(t *testing.T) {
+	t.Run("successfully create an instance of MedicalRecordSelector", func(t *testing.T) {
+		exec := createMedicalRecordSelectorExecutor()
+		assert.NotNil(t, exec.repo)
+	})
+}
+
+func createMedicalRecordSelectorExecutor() *MedicalRecordSelector_Executor {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		log.Panicf("[createMedicalRecordSelectorExecutor] error opening a stub database connection: %v\n", err)
+	}
+
+	repo := repository.NewMedicalRecordSelector(db)
+	return &MedicalRecordSelector_Executor{
+		repo: repo,
+		sql:  mock,
+	}
+}
