4274647a20b5635a9f88dd424f964dab46423b6d
Indra Saputra <flame.fire99@gmail.com>
2021-01-28 15:49:32 +0700

    implement find by email in handler

--- internal/http/handler/medical_record_finder.go
+++ internal/http/handler/medical_record_finder.go
@@ -1,6 +1,13 @@
 package handler
 
-import "github.com/orvosi/api/usecase"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/orvosi/api/entity"
+	"github.com/orvosi/api/internal/http/response"
+	"github.com/orvosi/api/usecase"
+)
 
 // MedicalRecordFinder handles HTTP request and response
 // for find medical record.
@@ -13,4 +20,29 @@ func NewMedicalRecordFinder(finder usecase.FindMedicalRecord) *MedicalRecordFind
 	return &MedicalRecordFinder{
 		finder: finder,
 	}
+}
+
+// FindByEmail handles `GET /medical-records` endpoint.
+// It extracts the user's email from bearer token
+// then finds all medical records bounded to the user.
+func (mf *MedicalRecordFinder) FindByEmail(ctx echo.Context) error {
+	user, err := extractUserFromRequestContext(ctx.Request().Context())
+	if err != nil {
+		res := response.NewError(err)
+		ctx.JSON(http.StatusInternalServerError, res)
+		return err
+	}
+
+	res, ferr := mf.finder.FindByEmail(ctx.Request().Context(), user.Email)
+	if ferr != nil {
+		status := http.StatusInternalServerError
+		if ferr.Code != entity.ErrInternalServer.Code {
+			status = http.StatusBadRequest
+		}
+		ctx.JSON(status, res)
+		return ferr
+	}
+
+	ctx.JSON(http.StatusOK, response.NewSuccess(res, response.EmptyMeta{}))
+	return nil
 }
