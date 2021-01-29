ed956b0290fbd54051fd0cfe83a5ef4262023079
Indra Saputra <flame.fire99@gmail.com>
2021-01-29 09:41:12 +0700

    implement findbyid in repository

--- internal/repository/medical_record_selector.go
+++ internal/repository/medical_record_selector.go
@@ -19,6 +19,28 @@ func NewMedicalRecordSelector(db *sql.DB) *MedicalRecordSelector {
 	return &MedicalRecordSelector{db: db}
 }
 
+// FindByID finds medical record by its id.
+func (ms *MedicalRecordSelector) FindByID(ctx context.Context, id uint64) ([]*entity.MedicalRecord, *entity.Error) {
+	query := "SELECT id, symptom, diagnosis, therapy, result, updated_at FROM medical_records WHERE id = $1 LIMIT 1"
+	rows, err := ms.db.QueryContext(ctx, query, id)
+	if err != nil {
+		return []*entity.MedicalRecord{}, entity.WrapError(entity.ErrInternalServer, err.Error())
+	}
+	defer rows.Close()
+
+	var result []*entity.MedicalRecord
+	for rows.Next() {
+		var tmp entity.MedicalRecord
+		if err := rows.Scan(&tmp.ID, &tmp.Symptom, &tmp.Diagnosis, &tmp.Therapy, &tmp.Result, &tmp.UpdatedAt); err != nil {
+			log.Printf("[MedicalRecordSelector-FindByID] scan rows error: %v", err)
+			continue
+		}
+
+		result = append(result, &tmp)
+	}
+	return result, nil
+}
+
 // FindByEmail finds all medical records bounded to specific email.
 func (ms *MedicalRecordSelector) FindByEmail(ctx context.Context, email string) ([]*entity.MedicalRecord, *entity.Error) {
 	query := "SELECT id, symptom, diagnosis, therapy, result, updated_at FROM medical_records WHERE email = $1 ORDER BY id ASC"
