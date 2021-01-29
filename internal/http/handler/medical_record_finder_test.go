242b44b04f7ba906e88bd0002ee0802002d9d10d
Indra Saputra <flame.fire99@gmail.com>
2021-01-29 08:02:59 +0700

    test when request doesnt contain user info

--- internal/http/handler/medical_record_finder_test.go
+++ internal/http/handler/medical_record_finder_test.go
@@ -1,9 +1,13 @@
 package handler_test
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	"github.com/labstack/echo/v4"
 	"github.com/orvosi/api/internal/http/handler"
 	mock_usecase "github.com/orvosi/api/test/mock/usecase"
 	"github.com/stretchr/testify/assert"
@@ -21,6 +25,26 @@ func TestNewMedicalRecordFinder(t *testing.T) {
 	t.Run("successfully create an instance of MedicalRecordFinder", func(t *testing.T) {
 		exec := createMedicalRecordFinderExecutor(ctrl)
 		assert.NotNil(t, exec.handler)
+	})
+}
+
+func TestMedicalRecordFinder_FindByEmail(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	t.Run("can't extract user information from request context", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/medical-records", nil)
+
+		rec := httptest.NewRecorder()
+		e := echo.New()
+		ctx := e.NewContext(req, rec)
+
+		exec := createMedicalRecordFinderExecutor(ctrl)
+		exec.handler.FindByEmail(ctx)
+
+		assert.Equal(t, http.StatusInternalServerError, rec.Code)
+		str := fmt.Sprintf("%s\n", `{"errors":[{"code":"01-001","message":"Internal server error"}],"meta":null}`)
+		assert.Equal(t, str, rec.Body.String())
 	})
 }
 
