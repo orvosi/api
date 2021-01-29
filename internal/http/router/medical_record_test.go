adddc3f0fff01c5b7215f04d73446245891a7bd9
Indra Saputra <flame.fire99@gmail.com>
2021-01-29 08:26:53 +0700

    test medical record finder route

--- internal/http/router/medical_record_test.go
+++ internal/http/router/medical_record_test.go
@@ -28,7 +28,30 @@ func TestMedicalRecordCreatorRoutes(t *testing.T) {
 	})
 }
 
+func TestMedicalRecordFinderRoutes(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	t.Run("all desired medical record finder routes are registered", func(t *testing.T) {
+		desired := map[string]string{
+			"/medical-records": "GET",
+		}
+
+		h := createMedicalRecordFinder(ctrl)
+		routes := router.MedicalRecordFinder(h)
+
+		for _, route := range routes {
+			assert.Equal(t, desired[route.Path], route.Method)
+		}
+	})
+}
+
 func createMedicalRecordCreator(ctrl *gomock.Controller) *handler.MedicalRecordCreator {
 	m := mock_usecase.NewMockCreateMedicalRecord(ctrl)
 	return handler.NewMedicalRecordCreator(m)
+}
+
+func createMedicalRecordFinder(ctrl *gomock.Controller) *handler.MedicalRecordFinder {
+	m := mock_usecase.NewMockFindMedicalRecord(ctrl)
+	return handler.NewMedicalRecordFinder(m)
 }
