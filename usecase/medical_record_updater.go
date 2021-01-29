112dcfb93a608d971ca8bc9cd1f1f318fb760dc6
Indra Saputra <flame.fire99@gmail.com>
2021-01-29 17:36:15 +0700

    create update medical record storage interface

--- usecase/medical_record_updater.go
+++ usecase/medical_record_updater.go
@@ -13,6 +13,15 @@ type UpdateMedicalRecord interface {
 	Update(ctx context.Context, record *entity.MedicalRecord) *entity.Error
 }
 
+// UpdateMedicalRecordStorage defines the business logic
+// to update a medical record into a storage.
+type UpdateMedicalRecordStorage interface {
+	// FindByID finds medical records by its id.
+	FindByIDAndEmail(ctx context.Context, id uint64, email string) (*entity.MedicalRecord, *entity.Error)
+	// Update updates certain medical record.
+	Update(ctx context.Context, id uint64, record *entity.MedicalRecord) *entity.Error
+}
+
 // MedicalRecordUpdater responsibles for medical record update workflow.
 type MedicalRecordUpdater struct {
 }
@@ -20,4 +29,9 @@ type MedicalRecordUpdater struct {
 // NewMedicalRecordUpdater creates an instance of MedicalRecordUpdater.
 func NewMedicalRecordUpdater() *MedicalRecordUpdater {
 	return &MedicalRecordUpdater{}
+}
+
+// Update updates the medical record.
+func (mu *MedicalRecordUpdater) Update(ctx context.Context, record *entity.MedicalRecord, email string) *entity.Error {
+	return nil
 }
