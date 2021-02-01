874f2ee7bb4bd01e8f386f5ff5a77711ea2c6635
Indra Saputra <flame.fire99@gmail.com>
2021-02-01 16:01:31 +0700

    test when medical record id is invalid hashids

--- internal/http/handler/medical_record_updater_test.go
+++ internal/http/handler/medical_record_updater_test.go
@@ -1,9 +1,13 @@
 package handler_test
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	"github.com/labstack/echo/v4"
 	"github.com/orvosi/api/internal/http/handler"
 	mock_usecase "github.com/orvosi/api/test/mock/usecase"
 	"github.com/stretchr/testify/assert"
@@ -21,6 +25,29 @@ func TestNewMedicalRecordUpdater(t *testing.T) {
 	t.Run("successfully create an instance of MedicalRecordUpdater", func(t *testing.T) {
 		exec := createMedicalRecordUpdaterExecutor(ctrl)
 		assert.NotNil(t, exec.handler)
+	})
+}
+
+func TestMedicalRecordUpdater_Update(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	t.Run("entity id is not hashids.ID", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodPut, "/", nil)
+
+		rec := httptest.NewRecorder()
+		e := echo.New()
+		ctx := e.NewContext(req, rec)
+		ctx.SetPath("/medical-records/:id")
+		ctx.SetParamNames("id")
+		ctx.SetParamValues("1234")
+
+		exec := createMedicalRecordUpdaterExecutor(ctrl)
+		exec.handler.Update(ctx)
+
+		assert.Equal(t, http.StatusInternalServerError, rec.Code)
+		str := fmt.Sprintf("%s\n", `{"errors":[{"code":"01-004","message":"Entity ID is invalid"}],"meta":null}`)
+		assert.Equal(t, str, rec.Body.String())
 	})
 }
 
