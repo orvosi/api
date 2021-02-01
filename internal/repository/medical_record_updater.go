7b16e9361f5ad97461c1fee539ff9305e1c3197e
Indra Saputra <flame.fire99@gmail.com>
2021-02-01 13:44:18 +0700

    implement checking if record exists

--- internal/repository/medical_record_updater.go
+++ internal/repository/medical_record_updater.go
@@ -1,6 +1,11 @@
 package repository
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+
+	"github.com/orvosi/api/entity"
+)
 
 // MedicalRecordUpdater connects the database with medical record entity
 // and only responsible for usecase of updating a data.
@@ -11,4 +16,19 @@ type MedicalRecordUpdater struct {
 // NewMedicalRecordUpdater creates an instance of MedicalRecordUpdater.
 func NewMedicalRecordUpdater(db *sql.DB) *MedicalRecordUpdater {
 	return &MedicalRecordUpdater{db: db}
+}
+
+// DoesRecordExist checks whether medical record which has certain id and email exists.
+func (mu *MedicalRecordUpdater) DoesRecordExist(ctx context.Context, id uint64, email string) (bool, *entity.Error) {
+	query := "SELECT id FROM medical_records WHERE id = $1 AND email = $2 LIMIT 1"
+	row := mu.db.QueryRowContext(ctx, query, id, email)
+
+	var tmp uint64
+	err := row.Scan(&tmp)
+	if err != nil && err != sql.ErrNoRows {
+		return false, entity.WrapError(entity.ErrInternalServer, err.Error())
+	} else if err != nil && err == sql.ErrNoRows {
+		return false, nil
+	}
+	return true, nil
 }
