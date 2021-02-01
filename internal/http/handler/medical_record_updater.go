95447e91a0fe52c62c321f468d5ceed62c20b348
Indra Saputra <flame.fire99@gmail.com>
2021-02-01 15:56:32 +0700

    implement update medical record in handler side

--- internal/http/handler/medical_record_updater.go
+++ internal/http/handler/medical_record_updater.go
@@ -1,6 +1,14 @@
 package handler
 
-import "github.com/orvosi/api/usecase"
+import (
+	"net/http"
+
+	"github.com/indrasaputra/hashids"
+	"github.com/labstack/echo/v4"
+	"github.com/orvosi/api/entity"
+	"github.com/orvosi/api/internal/http/response"
+	"github.com/orvosi/api/usecase"
+)
 
 // UpdateMedicalRecordRequest represents medical record request.
 type UpdateMedicalRecordRequest struct {
@@ -20,5 +28,54 @@ type MedicalRecordUpdater struct {
 func NewMedicalRecordUpdater(updater usecase.UpdateMedicalRecord) *MedicalRecordUpdater {
 	return &MedicalRecordUpdater{
 		updater: updater,
+	}
+}
+
+// Update handles `PUT /medical-records/:id` endpoint.
+func (mru *MedicalRecordUpdater) Update(ctx echo.Context) error {
+	str := ctx.Param("id")
+	id, herr := hashids.DecodeHash([]byte(str))
+	if herr != nil {
+		res := response.NewError(entity.ErrInvalidID)
+		ctx.JSON(http.StatusInternalServerError, res)
+		return herr
+	}
+
+	var request UpdateMedicalRecordRequest
+	if err := ctx.Bind(&request); err != nil {
+		res := response.NewError(entity.ErrInvalidMedicalRecordRequest)
+		ctx.JSON(http.StatusBadRequest, res)
+		return err
+	}
+
+	user, err := extractUserFromRequestContext(ctx.Request().Context())
+	if err != nil {
+		res := response.NewError(err)
+		ctx.JSON(http.StatusInternalServerError, res)
+		return err
+	}
+
+	record := createMedicalRecordFromUpdateRequest(&request, user)
+	if err := mru.updater.Update(ctx.Request().Context(), uint64(id), record); err != nil {
+		res := response.NewError(err)
+		status := http.StatusNotFound
+		if err.Code == entity.ErrInternalServer.Code {
+			status = http.StatusInternalServerError
+		}
+		ctx.JSON(status, res)
+		return err
+	}
+
+	ctx.JSON(http.StatusOK, response.NewSuccess(nil, response.EmptyMeta{}))
+	return nil
+}
+
+func createMedicalRecordFromUpdateRequest(req *UpdateMedicalRecordRequest, user *entity.User) *entity.MedicalRecord {
+	return &entity.MedicalRecord{
+		User:      user,
+		Symptom:   req.Symptom,
+		Diagnosis: req.Diagnosis,
+		Therapy:   req.Therapy,
+		Result:    req.Result,
 	}
 }
