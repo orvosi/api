ae11e708d52cacf50370dadd4ff379299a5c33b1
Indra Saputra <flame.fire99@gmail.com>
2021-02-01 13:51:34 +0700

    test when query select returns error

--- internal/repository/medical_record_updater_test.go
+++ internal/repository/medical_record_updater_test.go
@@ -1,10 +1,13 @@
 package repository_test
 
 import (
+	"context"
+	"errors"
 	"log"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/orvosi/api/entity"
 	"github.com/orvosi/api/internal/repository"
 	"github.com/stretchr/testify/assert"
 )
@@ -18,6 +21,20 @@ func TestNewMedicalRecordUpdater(t *testing.T) {
 	t.Run("successfully create an instance of MedicalRecordUpdater", func(t *testing.T) {
 		exec := createMedicalRecordUpdaterExecutor()
 		assert.NotNil(t, exec.repo)
+	})
+}
+
+func TestMedicalRecordUpdater_DoesRecordExist(t *testing.T) {
+	t.Run("query returns internal error", func(t *testing.T) {
+		exec := createMedicalRecordUpdaterExecutor()
+
+		exec.sql.ExpectQuery(`SELECT id FROM medical_records WHERE id = \$1 AND email = \$2 LIMIT 1`).
+			WillReturnError(errors.New("fail to select from database"))
+		found, err := exec.repo.DoesRecordExist(context.Background(), uint64(1), "dummy@dummy.com")
+
+		assert.NotNil(t, err)
+		assert.Equal(t, entity.ErrInternalServer, err)
+		assert.False(t, found)
 	})
 }
 
