9aa59df2c5ae9dc0c19c5978f00ba92b6ccf1c50
Indra Saputra <flame.fire99@gmail.com>
2021-02-02 15:41:22 +0700

    implement sign in handler

--- internal/http/handler/sign_in.go
+++ internal/http/handler/sign_in.go
@@ -1,6 +1,13 @@
 package handler
 
-import "github.com/orvosi/api/usecase"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/orvosi/api/entity"
+	"github.com/orvosi/api/internal/http/response"
+	"github.com/orvosi/api/usecase"
+)
 
 // Signer handles HTTP request and response
 // for sign in.
@@ -13,4 +20,27 @@ func NewSigner(signin usecase.SignIn) *Signer {
 	return &Signer{
 		signin: signin,
 	}
+}
+
+// SignIn handles `POST /sign-in` endpoint.
+func (s *Signer) SignIn(ctx echo.Context) error {
+	user, err := extractUserFromRequestContext(ctx.Request().Context())
+	if err != nil {
+		res := response.NewError(err)
+		ctx.JSON(http.StatusInternalServerError, res)
+		return err
+	}
+
+	if err := s.signin.SignIn(ctx.Request().Context(), user); err != nil {
+		res := response.NewError(err)
+		status := http.StatusInternalServerError
+		if err.Code != entity.ErrEmptyUser.Code {
+			status = http.StatusBadRequest
+		}
+		ctx.JSON(status, res)
+		return err
+	}
+
+	ctx.JSON(http.StatusCreated, response.NewSuccess(nil, response.EmptyMeta{}))
+	return nil
 }
