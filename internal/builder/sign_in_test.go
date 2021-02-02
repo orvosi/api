5026f795845addbdd7a322e259ab01dc3edd3802
Indra Saputra <flame.fire99@gmail.com>
2021-02-02 16:49:25 +0700

    test signer builder

--- internal/builder/sign_in_test.go
+++ internal/builder/sign_in_test.go
@@ -1,1 +1,22 @@
 package builder_test
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/orvosi/api/internal/builder"
+	"github.com/orvosi/api/internal/config"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuildrSigner(t *testing.T) {
+	t.Run("successfully build signer", func(t *testing.T) {
+		cfg, err := config.NewConfig("../../test/fixture/env.valid")
+		assert.Nil(t, err)
+
+		db := &sql.DB{}
+
+		routes := builder.BuildSigner(cfg, db)
+		assert.NotEmpty(t, routes)
+	})
+}
