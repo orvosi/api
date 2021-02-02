ee6544a90a259738006098e2f782639967f17b25
Indra Saputra <flame.fire99@gmail.com>
2021-02-02 16:44:41 +0700

    test signer route

--- internal/http/router/sign_in_test.go
+++ internal/http/router/sign_in_test.go
@@ -1,1 +1,34 @@
 package router_test
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/orvosi/api/internal/http/handler"
+	"github.com/orvosi/api/internal/http/router"
+	mock_usecase "github.com/orvosi/api/test/mock/usecase"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSignerRoutes(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	t.Run("all desired signer routes are registered", func(t *testing.T) {
+		desired := map[string]string{
+			"/sign-in": "POST",
+		}
+
+		h := createSigner(ctrl)
+		routes := router.Signer(h)
+
+		for _, route := range routes {
+			assert.Equal(t, desired[route.Path], route.Method)
+		}
+	})
+}
+
+func createSigner(ctrl *gomock.Controller) *handler.Signer {
+	m := mock_usecase.NewMockSignIn(ctrl)
+	return handler.NewSigner(m)
+}
